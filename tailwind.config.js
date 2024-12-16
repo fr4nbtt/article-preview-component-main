a529566ffa58dbfa4a09100c1baae1c031164f07
fr4nbtt <batet.francisco@gmail.com>
2024-12-16 18:10:52 -0300

    Initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./css/**/*.css", "./js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
