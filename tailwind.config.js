4326873f69d885104088448d3b440232c0e7fbc7
fr4nbtt <batet.francisco@gmail.com>
2024-12-18 21:05:11 -0300

    Add Manrope font, refactor styles using @apply

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./index.html", "./css/**/*.css", "./js/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "very-dark-grayish-blue": "hsl(217, 19%, 35%)",
+        "desaturated-dark-blue": "hsl(214, 17%, 51%)",
+        "grayish-blue": "hsl(212, 23%, 69%)",
+        "light-grayish-blue": "hsl(210, 46%, 95%)",
+      },
+      fontSize: {
+        body: "13px",
+      },
+      fontWeight: {
+        medium: 500,
+        bold: 700,
+      },
+      fontFamily: {
+        manrope: ["Manrope", "serif"],
+      },
+    },
   },
   plugins: [],
 };
